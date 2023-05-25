2e2507e3ac10b16f2118620c57b8f3d60f39f5e6
volnet <eeee6688@hotmail.com>
2023-05-25 12:41:42 +0800

    代码分离.防止重复.SplitChunksPlugin

--- 05-code-splitting/webpack.config.js
+++ 05-code-splitting/webpack.config.js
@@ -4,15 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: {
-      import: './src/index.js',
-      dependOn: 'shared',
-    },
-    another: {
-      import: './src/another-module.js',
-      dependOn: 'shared',
-    },
-    shared: 'lodash',
+    index: './src/index.js',
+    another: './src/another-module.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -25,6 +18,8 @@ module.exports = {
     clean: true,
   },
   optimization: {
-    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
