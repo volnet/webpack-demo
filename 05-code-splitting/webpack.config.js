7da3728cc46fdda833008b41c0435d0effef655b
volnet <eeee6688@hotmail.com>
2023-05-25 12:29:03 +0800

    代码分离.入口起点(entry point)

--- 05-code-splitting/webpack.config.js
+++ 05-code-splitting/webpack.config.js
@@ -1,28 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
+    another: './src/another-module.js',
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
