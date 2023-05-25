62aff74d9042f101610998e336b87f6e515b508a
volnet <eeee6688@hotmail.com>
2023-05-25 12:37:34 +0800

    代码分离.防止重复.入口依赖

--- 05-code-splitting/webpack.config.js
+++ 05-code-splitting/webpack.config.js
@@ -1,13 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    another: './src/another-module.js',
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared',
+    },
+    another: {
+      import: './src/another-module.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'code-splitting',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
