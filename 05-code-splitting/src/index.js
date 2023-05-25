95d8ef396c7e0e3d788b62b6eacb8b115f4831b3
volnet <eeee6688@hotmail.com>
2023-05-25 12:50:50 +0800

    代码分离.动态导入(dynamic import)

--- 05-code-splitting/src/index.js
+++ 05-code-splitting/src/index.js
@@ -1,12 +1,22 @@
-import _ from 'lodash';
+/*function getComponent() {
+    return import('lodash')
+        .then(({ default: _ }) => {
+            const element = document.createElement('div');
 
-function component() {
-const element = document.createElement('div');
+            element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+            return element;
+        })
+        .catch((error) => 'An error occurred while loading the component');
+}
+*/
 
-// lodash（目前通过一个 script 引入）对于执行这一行是必需的
-element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-return element;
+async function getComponent() {
+    const element = document.createElement('div');
+    const { default: _ } = await import('lodash');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);动态导入(dynamic import)
+});
